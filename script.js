41e9f413662576105372b3e3fe0592ad2c73f09e
ariasg7 <gianiarip2002@hotmail.com>
2025-05-05 18:01:50 -0400

    Update

--- script.js
+++ script.js
@@ -8,3 +8,11 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     });
   });
 });
+
+// Hamburger Menu Toggle
+const hamburger = document.getElementById('hamburger');
+const navLinks = document.getElementById('nav-links');
+
+hamburger.addEventListener('click', () => {
+  navLinks.classList.toggle('show');
+});
